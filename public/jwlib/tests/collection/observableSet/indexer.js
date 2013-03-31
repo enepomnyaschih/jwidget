4c27080058ddf861af07b5e1f0b48d5fa35d6f84
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2013-03-31 17:29:28 +0700

    #13: Added JW.ObservableSet.Indexer unobservable target and multi source tests.

--- public/jwlib/tests/collection/observableSet/indexer.js
+++ public/jwlib/tests/collection/observableSet/indexer.js
@@ -18,7 +18,51 @@
 */
 
 JW.Tests.Collection.ObservableSet.IndexerTestCase = JW.Unit.TestCase.extend({
-	testMapper: function() {
+	testUnobservable: function() {
+		var d = new JW.Proxy("d");
+		var source = new JW.ObservableSet([ d ]);
+		var target = new JW.Map();
+		
+		var indexer = this.createIndexer(source, target);
+		this.assertTarget({ "d": d }, target);
+		
+		var f = new JW.Proxy("f");
+		source.addAll([ f ]);
+		this.assertTarget({ "d": d, "f": f }, target);
+		
+		var c = new JW.Proxy("c");
+		source.add(c);
+		this.assertTarget({ "d": d, "f": f, "c": c }, target);
+		
+		var b = new JW.Proxy("b");
+		var m = new JW.Proxy("m");
+		source.addAll([ b, m ]);
+		this.assertTarget({ "d": d, "f": f, "c": c, "b": b, "m": m }, target);
+		
+		source.addAll([]);
+		this.assertTarget({ "d": d, "f": f, "c": c, "b": b, "m": m }, target);
+		
+		source.remove(m);
+		this.assertTarget({ "d": d, "f": f, "c": c, "b": b }, target);
+		
+		source.remove(m);
+		this.assertTarget({ "d": d, "f": f, "c": c, "b": b }, target);
+		
+		source.clear();
+		this.assertTarget({}, target);
+		
+		var h = new JW.Proxy("h");
+		source.add(h);
+		this.assertTarget({ "h": h }, target);
+		
+		indexer.destroy();
+		this.assertTarget({}, target);
+		
+		target.destroy();
+		source.destroy();
+	},
+	
+	testObservable: function() {
 		var d = new JW.Proxy("d");
 		var source = new JW.ObservableSet([ d ]);
 		var target = this.createTarget();
@@ -102,10 +146,102 @@ JW.Tests.Collection.ObservableSet.IndexerTestCase = JW.Unit.TestCase.extend({
 			"Changed size from 1 to 0"
 		);
 		indexer.destroy();
+		this.assertTarget({}, target);
+		
+		this.setExpectedOutput();
 		target.destroy();
 		source.destroy();
 	},
 	
+	testMultiSource: function() {
+		var a = new JW.Proxy("a");
+		var b = new JW.Proxy("b");
+		var c = new JW.Proxy("c");
+		var d = new JW.Proxy("d");
+		var x = new JW.Proxy("x");
+		
+		var source1 = new JW.ObservableSet([ a, b ]);
+		var source2 = new JW.ObservableSet([ c, d ]);
+		var target = this.createTarget();
+		
+		this.setExpectedOutput(
+			"Added x at x",
+			"Changed",
+			"Changed size from 0 to 1"
+		);
+		target.set(x, "x");
+		this.assertTarget({ "x": x }, target);
+		
+		this.setExpectedOutput(
+			"Added a at a",
+			"Added b at b",
+			"Changed",
+			"Changed size from 1 to 3"
+		);
+		var indexer1 = this.createIndexer(source1, target);
+		this.assertTarget({ "a": a, "b": b, "x": x }, target);
+		
+		this.setExpectedOutput(
+			"Added c at c",
+			"Added d at d",
+			"Changed",
+			"Changed size from 3 to 5"
+		);
+		var indexer2 = this.createIndexer(source2, target);
+		this.assertTarget({ "a": a, "b": b, "c": c, "d": d, "x": x }, target);
+		
+		var e = new JW.Proxy("e");
+		this.setExpectedOutput(
+			"Added e at e",
+			"Changed",
+			"Changed size from 5 to 6"
+		);
+		source1.add(e);
+		this.assertTarget({ "a": a, "b": b, "c": c, "d": d, "e": e, "x": x }, target);
+		
+		this.setExpectedOutput(
+			"Removed d at d",
+			"Changed",
+			"Changed size from 6 to 5"
+		);
+		source2.remove(d);
+		this.assertTarget({ "a": a, "b": b, "c": c, "e": e, "x": x }, target);
+		
+		this.setExpectedOutput(
+			"Removed a at a",
+			"Removed b at b",
+			"Removed e at e",
+			"Changed",
+			"Changed size from 5 to 2"
+		);
+		source1.clear();
+		this.assertTarget({ "c": c, "x": x }, target);
+		
+		this.setExpectedOutput(
+			"Removed c at c",
+			"Changed",
+			"Changed size from 2 to 1"
+		);
+		indexer2.destroy();
+		this.assertTarget({ "x": x }, target);
+		
+		this.setExpectedOutput();
+		indexer1.destroy();
+		this.assertTarget({ "x": x }, target);
+		
+		this.setExpectedOutput(
+			"Removed x at x",
+			"Changed",
+			"Changed size from 1 to 0"
+		);
+		target.destroy();
+		
+		this.setExpectedOutput();
+		source1.destroy();
+		source2.destroy();
+	},
+	
+	// tests that empty source doesn't caused target to trigger "change" on synchronizer initialization
 	testEmptyChange: function() {
 		var source = new JW.ObservableSet();
 		var target = this.createTarget();
@@ -119,6 +255,7 @@ JW.Tests.Collection.ObservableSet.IndexerTestCase = JW.Unit.TestCase.extend({
 		var d = new JW.Proxy("d");
 		var source = new JW.ObservableSet([ d ]);
 		var indexer = this.createIndexer(source);
+		this.assertTrue(indexer.target instanceof JW.ObservableMap);
 		this.assertTarget({ "d": d }, indexer.target);
 		indexer.destroy();
 		source.destroy();
@@ -158,6 +295,6 @@ JW.Tests.Collection.ObservableSet.IndexerTestCase = JW.Unit.TestCase.extend({
 	},
 	
 	assertTarget: function(expected, target) {
-		this.assertTrue(JW.Map.equal(expected, target.map.json));
+		this.assertTrue(JW.Map.equal(expected, target.getJson()));
 	}
 });
