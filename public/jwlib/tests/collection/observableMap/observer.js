5f4edcf1779354900227403073291227d6deb357
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2013-04-09 20:24:29 +0700

    #14: Added TDD tests.

---
+++ public/jwlib/tests/collection/observableMap/observer.js
@@ -0,0 +1,104 @@
+﻿/*
+	jWidget Lib tests.
+	
+	Copyright (C) 2013 Egor Nepomnyaschih
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+JW.Tests.Collection.ObservableMap.ObserverTestCase = JW.Unit.TestCase.extend({
+	testObserver: function() {
+		var source = new JW.ObservableMap({ "d": "D" });
+		
+		this.setExpectedOutput(
+			"Added D"
+		);
+		var syncher = source.createObserver({
+			addItem    : function(item) { this.output("Added " + item); },
+			removeItem : function(item) { this.output("Removed " + item); },
+			clearItems : function(items) { this.output("Cleared " + items.join(", ")); },
+			scope      : this
+		});
+		
+		this.setExpectedOutput(
+			"Added F"
+		);
+		source.setAll({ "f": "F" });
+		
+		this.setExpectedOutput(
+			"Added C"
+		);
+		source.set("C", "c");
+		
+		this.setExpectedOutput(
+			"Added B",
+			"Added M"
+		);
+		source.setAll({ "b": "B", "m": "M" });
+		
+		this.setExpectedOutput();
+		source.setAll({});
+		
+		this.setExpectedOutput(
+			"Removed M"
+		);
+		source.remove("m");
+		
+		this.setExpectedOutput();
+		source.remove("m");
+		
+		this.setExpectedOutput(
+			"Removed F",
+			"Added T"
+		);
+		source.set("T", "f");
+		
+		this.setExpectedOutput(
+			"Cleared D, T, C, B"
+		);
+		source.clear();
+		
+		this.setExpectedOutput(
+			"Added H"
+		);
+		source.set("H", "h");
+		
+		this.setExpectedOutput(
+			"Cleared H"
+		);
+		syncher.destroy();
+	},
+	
+	testAutoClear: function() {
+		var source = new JW.ObservableMap({ "a": "A", "b": "B", "c": "C" });
+		
+		this.setExpectedOutput(
+			"Added A",
+			"Added B",
+			"Added C"
+		);
+		var syncher = source.createObserver({
+			addItem    : function(item) { this.output("Added " + item); },
+			removeItem : function(item) { this.output("Removed " + item); },
+			scope      : this
+		});
+		
+		this.setExpectedOutput(
+			"Removed A",
+			"Removed B",
+			"Removed C"
+		);
+		syncher.destroy();
+	}
+});
---
+++ public/jwlib/tests/collection/observableSet/observer.js
@@ -0,0 +1,107 @@
+﻿/*
+	jWidget Lib tests.
+	
+	Copyright (C) 2013 Egor Nepomnyaschih
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+	
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+JW.Tests.Collection.ObservableSet.ObserverTestCase = JW.Unit.TestCase.extend({
+	testObserver: function() {
+		var d = new JW.Proxy("d");
+		var source = new JW.ObservableSet([ d ]);
+		
+		this.setExpectedOutput(
+			"Added d"
+		);
+		var syncher = source.createObserver({
+			addItem    : function(item) { this.output("Added " + item.value); },
+			removeItem : function(item) { this.output("Removed " + item.value); },
+			clearItems : function(items) { this.output("Cleared " + JW.Array.mapByField(items, "value").join(", ")); },
+			scope      : this
+		});
+		
+		var f = new JW.Proxy("f");
+		this.setExpectedOutput(
+			"Added f"
+		);
+		source.addAll([ f ]);
+		
+		var c = new JW.Proxy("c");
+		this.setExpectedOutput(
+			"Added c"
+		);
+		source.add(c);
+		
+		var b = new JW.Proxy("b");
+		var m = new JW.Proxy("m");
+		this.setExpectedOutput(
+			"Added b",
+			"Added m"
+		);
+		source.addAll([ b, m ]);
+		
+		this.setExpectedOutput();
+		source.addAll([]);
+		
+		this.setExpectedOutput(
+			"Removed m"
+		);
+		source.remove(m);
+		
+		this.setExpectedOutput();
+		source.remove(m);
+		
+		this.setExpectedOutput(
+			"Cleared d, f, c, b"
+		);
+		source.clear();
+		
+		var h = new JW.Proxy("h");
+		this.setExpectedOutput(
+			"Added h"
+		);
+		source.add(h);
+		
+		this.setExpectedOutput(
+			"Cleared h"
+		);
+		syncher.destroy();
+	},
+	
+	testAutoClear: function() {
+		var a = new JW.Proxy("a");
+		var b = new JW.Proxy("b");
+		var c = new JW.Proxy("c");
+		var source = new JW.ObservableSet([ a, b, c ]);
+		
+		this.setExpectedOutput(
+			"Added a",
+			"Added b",
+			"Added c"
+		);
+		var syncher = source.createObserver({
+			addItem    : function(item) { this.output("Added " + item.value); },
+			removeItem : function(item) { this.output("Removed " + item.value); },
+			scope      : this
+		});
+		
+		this.setExpectedOutput(
+			"Removed a",
+			"Removed b",
+			"Removed c"
+		);
+		syncher.destroy();
+	}
+});
