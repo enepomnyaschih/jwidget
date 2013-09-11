285a38a469903621e0ac50a4716cce6850af6096
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2013-09-11 19:54:53 +0700

    #45: Improved docs generating script.

--- docs.bat
+++ docs.bat
@@ -1,3 +1,11 @@
-rd /Q /S ..\docs
-md ..\docs
-jsduck-5.2.0 public/jw* --output ../docs
+@echo off
+rd /S /Q ..\docs\extjs
+rd /S /Q ..\docs\member-icons
+rd /S /Q ..\docs\output
+rd /S /Q ..\docs\resources
+rd /S /Q ..\docs\source
+del /Q ..\docs\*
+md ..\docs-temp
+jsduck-5.2.0 public/jw* --output ../docs-temp
+xcopy /I /Y /E /Q ..\docs-temp\* ..\docs
+rd /S /Q ..\docs-temp
