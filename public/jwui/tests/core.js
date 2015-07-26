d2c38fc3869b8635b54d7a15f9583b0bc9b0b5d4
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2015-07-26 18:13:21 +0600

    #111: Updated copyright year.

--- public/jwui/tests/core.js
+++ public/jwui/tests/core.js
@@ -1,7 +1,7 @@
 ﻿/*
 	jWidget UI tests.
 	
-	Copyright (C) 2014 Egor Nepomnyaschih
+	Copyright (C) 2015 Egor Nepomnyaschih
 	
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU Lesser General Public License as published by
