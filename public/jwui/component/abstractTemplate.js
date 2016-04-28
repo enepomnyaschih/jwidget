a228ede336c442727928d76e7123896d001b788e
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2016-04-28 23:06:04 +0600

    #137: Found a better (more efficient) solution for cyclic dependency problem resolution.

--- public/jwui/component/abstractTemplate.js
+++ public/jwui/component/abstractTemplate.js
@@ -22,7 +22,6 @@ JW.UI.Component.AbstractTemplate = function() {
 	this.prefixes = null; // Array<String>
 	this.parentIdMap = null; // Map<String, Map<String, true>>, the parent IDs of [key] item
 	this.childIdMap = null; // Map<String, Map<String, true>>, the child IDs of [key] item
-	this.rootIds = null; // Array<String>
 	this.ids = null; // Array<String>, in dependency order
 };
 
@@ -46,17 +45,20 @@ JW.extend(JW.UI.Component.AbstractTemplate, JW.Class, {
 		// resolving dependencies to a plain list of IDs
 		this.ids = [];
 		this._backtrace("root");
-		if (this.rootIds) {
-			JW.Array.each(this.rootIds, this._backtrace, this);
-		}
 
 		// check for trash
 		var remainingIds = JW.Map.getKeys(this.parentIdMap);
 		if (remainingIds.length !== 0) {
-			console.warn("jWidget template '" + this.prefixes.join(" ") +
-				"' has cyclic dependencies among the next jwid's: " + remainingIds.join(", ") +
-				". Can't detect the desired rendering order. Rendering elements in arbitrary order...");
-			this.ids.push.apply(this.ids, remainingIds);
+			// some ID's may not have been backtraced if they are assigned to the root element,
+			// so we must backtrace them to make sure that everything is processed
+			JW.Array.each(remainingIds, this._backtrace, this);
+			remainingIds = JW.Map.getKeys(this.parentIdMap);
+			if (remainingIds.length !== 0) {
+				console.warn("jWidget template '" + this.prefixes.join(" ") +
+					"' has cyclic dependencies among the next jwid's: " + remainingIds.join(", ") +
+					". Can't detect the desired rendering order. Rendering elements in arbitrary order...");
+				this.ids.push.apply(this.ids, remainingIds);
+			}
 		}
 
 		this.prefixes = null;
@@ -72,9 +74,6 @@ JW.extend(JW.UI.Component.AbstractTemplate, JW.Class, {
 				return null;
 			}
 			var ids = JW.String.parseClass(attr);
-			if (path.length === 0) {
-				this.rootIds = ids.concat();
-			}
 			el.removeAttribute("jwid");
 			var l = ids.length;
 			if (l === 0) {
