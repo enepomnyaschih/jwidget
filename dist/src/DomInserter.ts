aae8e9aa1e10e1aae7a9f35a667e62cf4063a5a9
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2017-04-11 21:54:15 +0600

    Applied LGPL v3 to source files.

--- dist/src/DomInserter.ts
+++ dist/src/DomInserter.ts
@@ -1,4 +1,24 @@
-﻿import AbstractInserter from './ui/AbstractInserter';
+﻿/*
+	jWidget 2
+	Copyright (C) 2017  Egor Nepomnyaschih
+	enepomnyaschih@gmail.com
+	https://github.com/enepomnyaschih/jwidget
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import AbstractInserter from './ui/AbstractInserter';
 
 /**
  * View synchronizer. Synchronizes DOM element children with the source array. Usually used in conjunction with
