81c9e265ac2c45fc53fb6ee68373972e917e6f47
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2018-05-26 16:18:55 +0600

    Documented Endpoint.

--- dist/src/Endpoint.ts
+++ dist/src/Endpoint.ts
@@ -1,13 +1,21 @@
 import {isNil} from ".";
 
+/**
+ * Signature of an endpoint resolution function. The function returns a specific URL by parameters. The endpoints
+ * can define routes to use with Router or backend API methods to access with request.
+ */
 interface Endpoint<P> {
-    (params: P): string;
+	(params: P): string;
 }
 
 export default Endpoint;
 
+/**
+ * Signature of an endpoint suffix resolution function. The functions returns a URL component or an array of URL
+ * components by parameters. The components get auto-encoded being passed to append function.
+ */
 export interface Suffix<P> {
-    (params: P): string | string[];
+	(params: P): string | string[];
 }
 
 function join(base: string, suffix: string[]): string {
@@ -27,11 +35,25 @@ function compute<P>(suffix: string | string[] | Suffix<P>, params: P): string[] 
     return typeof result === "string" ? [result] : result;
 }
 
+/**
+ * Defines a new endpoint by appending a suffix to an existing endpoint. If the suffix is defined as a function,
+ * the new endpoint may take additional parameters (because U should extend P).
+ * @param base Base endpoint to extend.
+ * @param suffix URI components to append to the endpoint or a function returning the components by parameters.
+ * @returns Endpoint representing the base endpoint with the suffix appended.
+ */
 export function append<P>(base: Endpoint<P>, suffix: string | string[]): Endpoint<P>;
 export function append<P, U extends P>(base: Endpoint<P>, suffix: Suffix<U>): Endpoint<U>;
 export function append<P, U extends P>(base: Endpoint<P>, suffix: string | string[] | Suffix<U>): Endpoint<U> {
     return (params: U) => join(base(params), compute(suffix, params));
 }
 
+/**
+ * Endpoint referring to "" (empty) URL. Use %append function to create new endpoints from this one.
+ */
 export const relativeRoot: Endpoint<{}> = () => "";
+
+/**
+ * Endpoint referring to "/" URL. Use %append function to create new endpoints from this one.
+ */
 export const absoluteRoot: Endpoint<{}> = () => "/";
