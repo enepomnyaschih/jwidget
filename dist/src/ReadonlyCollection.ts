4e593181cc938c9cdf3d45b692227c48271c1026
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2018-05-27 18:55:22 +0600

    Documented ReadonlyCollection.

--- dist/src/ReadonlyCollection.ts
+++ dist/src/ReadonlyCollection.ts
@@ -27,138 +27,7 @@ import Listenable from './Listenable';
 import Reducer from './Reducer';
 
 /**
- * Abstract collection.
- *
- * There are 3 collection types:
- *
- * * [[IList]],
- * extends [[IIndexedCollection]]
- * * [[IMap]],
- * extends [[IIndexedCollection]]
- * * [[ISet]]
- *
- * You can convert collections to each other using their methods.
- *
- * Each collection has 2 implementations:
- *
- * * Simple collections:
- * [[JWArray]],
- * [[JWMap]],
- * [[JWSet]]
- * * Observable collection:
- * [[ObservableArray]],
- * [[ObservableMap]],
- * [[ObservableSet]]
- *
- * The difference is that the observable collection triggers events about its modifications.
- * It lets you to synchronize view with data on fly in accordance to Model-View architecture.
- *
- * Internally, simple collections are very similar to native JavaScript collections.
- * But their API is identical to observable collections' (excepting lack of events).
- * So you can use simple collections as a bridge between native JavaScript collections and
- * jWidget observable collections.
- *
- * The next synchronizers exist to connect observable collections to each other:
- *
- * * [[ICollectionMapper]] - Item mapper
- * * [[ICollectionFilterer]] - Filterer
- * * [[ICollectionCounter]] - Counter by filter
- * * [[ICollectionLister]] - Converter to set
- * * [[ICollectionIndexer]] - Converter to map (indexer)
- * * [[ICollectionOrderer]] - Converter to array (orderer)
- * * [[ICollectionSorterComparing]] - Converter to array (sorter by comparer)
- * * [[ICollectionObserver]] - Modification observer
- * * [[IListInserter]], [[IMapInserter]] - View synchronizers
- * * [[IListMerger]] - Array merger
- * * [[IListReverser]] - Array reverser
- *
- * Please keep the next rules in mind whenever you work with jWidget collections.
- *
- * 1) The majority of collection modification methods have 2 implementations: **tryMethod** and **method**.
- * These methods perform the same collection modification but return different result.
- * **tryMethod** is introduced for internal use mainly,
- * and *it always returns undefined if collection has not been modified*.
- * For example, [[tryClear]] returns undefined if collection is empty,
- * else it returns old collection contents.
- * **method** returns result in more friendly format.
- * For example, [[clear]] always returns old collection contents.
- * So, if you want to clear collection and destroy all items, [[clear]] method fits better:
- *
- *     ArrayUtils.each(array.clear(), destroy); // correct
- *     ArrayUtils.each(array.tryClear(), destroy); // incorrect: 'undefined' exception if array is empty
- *
- * 2) The majority of collection returning methods have 2 implementations: **method** and **$method**.
- * These methods perform the same modification but return the result in different format.
- *
- * * **method** returns native JavaScript collection: Array or Object.
- * * **$method** returns jWidget collection: [[JWArray]],
- * [[JWMap]] or [[JWSet]].
- *
- * Please use the method that's more convenient in your specific situation.
- * For example, **$method** is convenient for chaining algorithm method calls.
- * So, previous example can become more readable with [[$clear]] method:
- *
- *     array.$clear().each(destroy);
- *
- * But in the next example [[clear]] is still suitable:
- *
- *     set.addAll(array.clear());
- *
- * 3) It is better if all items in collection are not null/undefined and are all unique. Some methods like
- * [[performReorder]] require each item to have an unique key.
- * If two items of collection are equal, then their keys are equal as well, so this method won't work correctly.
- *
- * # Collection methods
- *
- * Content retrieving:
- *
- * * [[length]] - Collection length property.
- * * [[isEmpty]] - Checks collection for emptiness.
- * * [[getFirst]] - Returns first item in collection.
- * * [[containsItem]] - Does collection contain the item?
- *
- * Iteration algorithms:
- *
- * * [[every]] - Checks all items by criteria.
- * Returns true if all items match the criteria.
- * * [[some]] - Checks each item by criteria.
- * Returns true if some item matches the criteria.
- * * [[each]], [[forEach]] - Iterates items through.
- * * [[search]] - Finds item by criteria.
- * Returns first item matching the criteria.
- * * [[filter]], [[filter]] - Filters collection by criteria.
- * Builds new collection of the same type, consisting of items matching the criteria.
- * * [[count]], [[$count]] - Counts the items matching criteria.
- * * [[map]], [[map]] - Maps collection items.
- * Builds new collection of the same type, consisting of results of mapping function call for each collection item.
- * * [[toSorted]], [[$toSorted]],
- * [[toSortedComparing]], [[$toSortedComparing]] -
- * Builds array consisting of collection items sorted by indexer or comparer.
- * * [[index]], [[$index]] - Indexes collection.
- * Builds new map by rule: key is the result of indexer function call, value is the corresponding item.
- * * [[toArray]], [[$toArray]] - Builds new array consisting of collection items.
- * * [[toSet]], [[toSet]] - Builds new set consisting of collection items.
- * * [[asArray]], [[$asArray]] - Represents collection as array.
- * * [[asSet]], [[asSet]] - Represents collection as set.
- *
- * Collection modification:
- *
- * * [[removeItem]] - Removes first occurency of an item in collection.
- * * [[removeItems]] - Removes all occurencies of items in collection.
- * * [[clear]], [[$clear]], [[tryClear]] - Clears collection.
- *
- * Similar collection creation (for algorithms and synchronizers implementation):
- *
- * * [[createEmpty]] - Creates empty collection of the same type.
- * * [[createEmptyArray]] - Creates empty array of the same observability type.
- * * [[createEmptyMap]] - Creates empty map of the same observability type.
- * * [[createEmptySet]] - Creates empty set of the same observability type.
- *
- * All the same algorithms are also available for native JavaScript collections:
- *
- * * Array, see [[ArrayUtils]] functions.
- * * Object as map, see [[DictionaryUtils]] functions.
- * * Object as set, see [[SetUtils]] functions.
+ * Abstract readonly collection.
  */
 interface ReadonlyCollection<T> {
 	/**
@@ -187,11 +56,7 @@ interface ReadonlyCollection<T> {
 	readonly first: T;
 
 	/**
-	 * Collection is cleared. Triggered in result of calling:
-	 *
-	 * * [[clear]]
-	 * * [[$clear]]
-	 * * [[tryClear]]
+	 * Collection is cleared.
 	 */
 	readonly clearEvent: Listenable<ICollection.EventParams<T>>;
 
@@ -201,7 +66,7 @@ interface ReadonlyCollection<T> {
 	readonly changeEvent: Listenable<ICollection.EventParams<T>>;
 
 	/**
-	 * Returns a full copy of this collection.
+	 * Returns a shallow copy of this collection.
 	 */
 	clone(): ICollection<T>;
 
@@ -211,185 +76,184 @@ interface ReadonlyCollection<T> {
 	contains(item: T): boolean;
 
 	/**
-	 * Matches all items against criteria.
-	 *
-	 * Returns true if callback returns !== false for all collection items.
-	 *
-	 * Algorithms iterates items sequentially, and stops after first item not matching the criteria.
-	 *
+	 * Matches all items against criteria. Returns true if callback returns truthy value for all collection
+	 * items. Algorithms iterates through the items sequentially, and stops it after the first item not matching
+	 * the criteria.
 	 * @param callback Criteria callback.
-	 * @param scope **callback** call scope. Defaults to collection itself.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @returns All items match the criteria.
 	 */
 	every(callback: (item: T) => any, scope?: any): boolean;
 
 	/**
-	 * Matches each item against criteria.
-	 *
-	 * Returns true if callback returns !== false for some collection item.
-	 *
-	 * Algorithms iterates items sequentially, and stops after first item matching the criteria.
-	 *
+	 * Matches each item against criteria. Returns true if callback returns truthy value for at least one item
+	 * in the collection. Algorithms iterates through the items sequentially, and stops it after the first item matching
+	 * the criteria.
 	 * @param callback Criteria callback.
-	 * @param scope **callback** call scope. Defaults to collection itself.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @returns Some item matches the criteria.
 	 */
 	some(callback: (item: T) => any, scope?: any): boolean;
 
 	/**
-	 * Iterates collection items. Calls specified function for all items.
-	 *
+	 * Iterates through the collection items. Calls the specified function for all items.
 	 * @param callback Callback function.
-	 * @param scope **callback** call scope. Defaults to collection itself.
+	 * @param scope Callback call scope. Defaults to the collection.
 	 */
 	forEach(callback: (item: T) => any, scope?: any): void;
 
 	/**
-	 * Finds item matching criteria.
-	 *
-	 * Returns first item for which callback returns !== false.
-	 *
-	 * Algorithms iterates items sequentially, and stops after first item matching the criteria.
-	 *
+	 * Finds an item matching the criteria. Returns the first item the callback returns truthy value for.
+	 * Algorithms iterates through the items sequentially, and stops it after the first item matching the criteria.
 	 * @param callback Criteria callback.
-	 * @param scope **callback** call scope. Defaults to collection itself.
-	 * @returns Found item or undefined.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @returns First item matching the criteria.
 	 */
 	find(callback: (item: T) => any, scope?: any): T;
 
 	/**
-	 * Converts collection to sorted array.
-	 *
-	 * Builds array consisting of collection items sorted by result of callback call for each item.
-	 *
-	 * @param callback Indexer function. Must return a comparable value, compatible with
-	 * [[cmp]]. Returns item itself by default.
-	 * @param scope **callback** call scope. Defaults to collection itself.
-	 * @param order Sorting order. Positive number for ascending sorting, negative for descending sorting.
+	 * Converts the collection to a sorted array. Builds an array consisting of the collection items sorted by
+	 * the result of the callback call for each item.
+	 * @param callback Indexer function. Must return a comparable value, compatible with cmp. Returns the item itself by default.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @param order Sorting order. Positive number for ascending sorting (default), negative number for descending sorting.
 	 * @returns Sorted array.
 	 */
 	toSorted(callback?: (item: T) => any, scope?: any, order?: number): IList<T>;
 
 	/**
-	 * Converts collection to sorted array.
-	 *
-	 * Builds array consisting of collection items sorted by comparer.
-	 *
+	 * Converts the collection to a sorted array. Builds an array consisting of the collection items sorted by comparer.
 	 * @param compare Comparer function. Should return positive value if t1 > t2;
-	 * negative value if t1 < t2; 0 if t1 == t2.
-	 * Defaults to [[cmp]]
-	 * @param scope **comparer** call scope. Defaults to collection itself.
-	 * @param order Sorting order. Positive number for ascending sorting, negative for descending sorting.
+	 * negative value if t1 < t2; 0 if t1 == t2. Defaults to `cmp`.
+	 * @param scope Callback call scope. Defaults to collection itself.
+	 * @param order Sorting order. Positive number for ascending sorting (default), negative number for descending sorting.
 	 * @returns Sorted array.
 	 */
 	toSortedComparing(compare?: (t1: T, t2: T) => number, scope?: any, order?: number): IList<T>;
 
 	/**
-	 * Indexes collection.
-	 *
-	 * Builds new map by rule: key is the result of indexer function call, value is the corresponding item.
-	 *
+	 * Indexes collection. Builds a new map by rule: key is the result of indexer function call, value is the corresponding item.
 	 * @param callback Indexer function.
-	 * @param scope **callback** call scope. Defaults to collection itself.
+	 * @param scope Callback call scope. Defaults to collection itself.
 	 * @returns Collection index.
 	 */
 	index(callback: (item: T) => any, scope?: any): IMap<T>;
 
 	/**
-	 * Converts collection to array.
-	 *
-	 * Builds new array consisting of collection items.
+	 * Converts collection to an array. Builds a new array consisting of collection items.
 	 */
 	toArray(): T[];
 
 	/**
-	 * Converts collection to array.
-	 *
-	 * Builds new array consisting of collection items.
+	 * Converts collection to a list. Builds a new list consisting of collection items.
 	 */
 	toList(): IList<T>;
 
 	/**
-	 * Converts collection to set.
-	 *
-	 * Builds new set consisting of collection items.
+	 * Converts collection to a set. Builds a new set consisting of collection items.
 	 */
 	toSet(): ISet<T>;
 
 	/**
 	 * Represents collection as array.
-	 *
-	 * If this collection is array, returns it immediately.
-	 * Else, executes [[toArray]] method.
-	 * This method works usually faster than [[toArray]],
-	 * but please make sure that the returned array
-	 * won't be modified externally, because it can cause strange unexpected bugs.
+	 * If this collection is list, returns the array of items immediately.
+	 * Else, executes toArray method.
+	 * Use with caution.
 	 */
 	asArray(): T[];
 
 	/**
-	 * Represents collection as array.
-	 *
-	 * If this collection is array, returns it immediately.
-	 * Else, executes [[toArray]] method.
-	 * This method works usually faster than [[toArray]],
-	 * but please make sure that the returned array
-	 * won't be modified externally, because it can cause strange unexpected bugs.
+	 * Represents collection as list.
+	 * If this collection is list, returns it immediately.
+	 * Else, executes toList method.
+	 * Use with caution.
 	 */
 	asList(): IList<T>;
 
 	/**
 	 * Represents collection as set.
-	 *
 	 * If this collection is set, returns it immediately.
-	 * Else, executes [[toSet]] method.
-	 * This method works usually faster than [[toSet]],
-	 * but please make sure that the returned set
-	 * won't be modified externally, because it can cause strange unexpected bugs.
+	 * Else, executes toSet method.
+	 * Use with caution.
 	 */
 	asSet(): ISet<T>;
 
 	/**
-	 * Filters collection by criteria.
-	 *
-	 * Builds new collection of the same type, consisting of items for which callback returns !== false.
-	 *
+	 * Filters the collection by criteria. Builds a new collection of the same type, consisting of items the callback
+	 * return truthy value for.
 	 * @param callback Criteria callback.
-	 * @param scope **callback** call scope. Defaults to collection itself.
+	 * @param scope Callback call scope. Defaults to the collection.
 	 * @returns Filtered collection.
 	 */
 	filter(callback: (item: T) => any, scope?: any): ICollection<T>;
 
 	/**
-	 * Counts the items matching criteria.
-	 *
-	 * Returns the number of items for which callback returns !== false.
-	 *
+	 * Counts the items matching criteria. Returns the number of items the callback returns truthy value for.
 	 * @param callback Criteria callback.
-	 * @param scope **callback** call scope. Defaults to collection itself.
-	 * @returns Number of items.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @returns Number of matching items.
 	 */
 	count(callback: (item: T) => any, scope?: any): number;
 
 	/**
 	 * Maps collection items.
-	 *
-	 * Builds new collection of the same type, containing results of callback call for each collection item.
-	 *
+	 * Builds a new collection of the same type, containing results of the callback call for each collection item.
 	 * @param callback Mapping function.
-	 * @param scope **callback** call scope. Defaults to collection itself.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @param getKey Function which returns unique key of an item in the resulting collection.
 	 * @returns Mapped collection.
 	 */
 	map<U>(callback: (item: T) => U, scope?: any, getKey?: (item: U) => any): ICollection<U>;
 
+	/**
+	 * Applies a function against an accumulator and each item in the collection to reduce it to a single value.
+	 * @param reducer Standard reducer.
+	 * @returns Final accumulator value.
+	 */
 	reduce<U>(reducer: Reducer<T, U>): U;
 
+	/**
+	 * Applies a function against an accumulator and each item in the collection to reduce it to a single value.
+	 * @param callback Function to execute on each item in the collection.
+	 * @param initial Value to use as the first argument to the first call of the callback.
+	 * @returns Final accumulator value.
+	 */
 	reduce<U>(callback: (accumulator: U, item: T) => U, initial: U): U;
 
+	/**
+	 * Returns the collection item the %callback returns the highest (or lowest if %order < 0) value for.
+	 * @param callback Returns a comparable value, compatible with %cmp. Returns the item itself by default.
+	 * @param scope Callback call scope. Defaults to the collection
+	 * @param order Pass negative order to find the lowest value.
+	 * @returns Maximum collection item.
+	 */
 	max(callback?: (item: T) => any, scope?: any, order?: number): T;
 
+	/**
+	 * Returns the highest (or lowest if order < 0) collection item in terms of the specified comparer function.
+	 * @param compare Returns a positive value if t1 > t2; negative value if t1 < t2; 0 if t1 == t2. Defaults to cmp.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @param order Pass negative order to find the lowest value.
+	 * @returns Maximum collection item.
+	 */
 	maxComparing(compare?: (t1: T, t2: T) => number, scope?: any, order?: number): T;
 
+	/**
+	 * Returns the collection item the %callback returns the lowest (or highest if %order < 0) value for.
+	 * @param callback Returns a comparable value, compatible with %cmp. Returns the item itself by default.
+	 * @param scope Callback call scope. Defaults to the collection
+	 * @param order Pass negative order to find the highest value.
+	 * @returns Minimum collection item.
+	 */
 	min(callback?: (item: T) => any, scope?: any, order?: number): T;
 
+	/**
+	 * Returns the lowest (or highest if order < 0) collection item in terms of the specified comparer function.
+	 * @param compare Returns a positive value if t1 > t2; negative value if t1 < t2; 0 if t1 == t2. Defaults to cmp.
+	 * @param scope Callback call scope. Defaults to the collection.
+	 * @param order Pass negative order to find the highest value.
+	 * @returns Minimum collection item.
+	 */
 	minComparing(compare?: (t1: T, t2: T) => number, scope?: any, order?: number): T;
 }
 
