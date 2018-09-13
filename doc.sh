de7ee369a3b29425d81f8d0dc37c2cb604c53e8f
Egor Nepomnyaschih <enepomnyaschih@gmail.com>
2018-09-13 17:13:20 +0200

    Finished documentation build script.

--- doc.sh
+++ doc.sh
@@ -22,11 +22,31 @@ pushd .. || exit 1
 rm -r doc/$1
 mv git/docoutput doc/$1 || exit 1
 
-pushd mt | exit 1
+pushd mt || exit 1
 git checkout mt-$1-1 || exit 1
 npm install || exit 1
 npm run build || exit 1
 mv public ../doc/$1/samples/tutorial1 || exit 1
+git checkout mt-$1-2 || exit 1
+npm install || exit 1
+npm run build || exit 1
+mv public ../doc/$1/samples/tutorial2 || exit 1
+git checkout mt-$1-3 || exit 1
+npm install || exit 1
+npm run build || exit 1
+mv public ../doc/$1/samples/tutorial3 || exit 1
+git checkout mt-$1-4 || exit 1
+npm install || exit 1
+npm run build || exit 1
+mv public ../doc/$1/samples/tutorial4 || exit 1
+git checkout mt-$1-5 || exit 1
+npm install || exit 1
+npm run build || exit 1
+mv public ../doc/$1/samples/tutorial5 || exit 1
+git checkout mt-$1-6 || exit 1
+npm install || exit 1
+npm run build || exit 1
+mv public ../doc/$1/samples/tutorial6 || exit 1
 popd
 
 popd
